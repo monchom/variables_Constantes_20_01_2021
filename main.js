3208b9f5b6e489adb536c12a90d9e93e1d310087
Adrian velasquez Guerrero <adrian.velasquez.guerrero@gmail.com>
2021-01-20 20:58:39 -0500

    variables y constantes_20_01_2021

---
+++ main.js
@@ -0,0 +1,48 @@
+console.log("Conectado");
+
+vaso = "Cafe";
+
+console.log(vaso);
+
+vaso = "Agua";
+
+console.log(vaso)
+
+
+nombre = "Adrian"
+
+edad = 50
+
+profesor = false
+
+lista_cursos = ['JavaScript','PHP',"Dart","Kotlin"]
+
+informacion_personal = {
+
+    id: 1921901910,
+    direccion: "Calle 103 # 15-81",
+    cursos: ['java',"dart",'Javascript']
+}
+
+console.log(nombre);
+console.log(edad);
+console.log(profesor);
+console.log(lista_cursos);
+console.log(informacion_personal);
+
+var nombre2 = "Adrian";
+let edad2 = 50;
+
+
+//Al momento de declarar la constante hay que inicializarla cp
+// Un valor 
+const cedula = 1019010101;
+
+// No se puede incluir un nuevo valor
+//cedula = 100;
+
+
+
+
+
+
